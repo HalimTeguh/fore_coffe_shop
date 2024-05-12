8e35d473a7e23e1a77919c273ab7d595246086ba
Halim Teguh S <halimteguhsaputro@gmail.com>
2024-05-12 09:08:18 +0700

    feat: :sparkles: Add Coffe Model

---
+++ lib/models/coffe.dart
@@ -0,0 +1,8 @@
+class Coffe{
+  final String name;
+  final int price;
+  final String description;
+  final String imagePath;
+
+  Coffe({required this.name, required this.price, required this.description, required this.imagePath});
+}
